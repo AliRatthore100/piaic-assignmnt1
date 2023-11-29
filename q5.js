5f7280bd8a60f10a205f66ec9af56e056af8943e
AliRatthore100 <ali_rathore10@outlook.com>
2023-11-29 10:35:37 -0800

    q5 ok

---
+++ q5.js
@@ -0,0 +1,3 @@
+var famousperson = "Qude azam";
+var message = "Once said,I do not believe in taking the right decision, I take a decision and make it right";
+console.log(famousperson + message);
---
+++ q5.ts
@@ -0,0 +1,3 @@
+let famousperson:string="Qude azam";
+let message:string="Once said,I do not believe in taking the right decision, I take a decision and make it right";
+console.log(famousperson+message)
